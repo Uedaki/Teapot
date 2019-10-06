f4e183408c3dc003cf0768f32fb3e18353c460bb
Uedaki <thiver.96@gmail.com>
2019-10-06 15:39:35 +0200

    configuration of cmakelist

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "Teapot.h"
+
+int main()
+{
+	std::cout << TEAPOT_PROJECT_NAME << " - version " << TEAPOT_VERSION_MAJOR << "." << TEAPOT_VERSION_MINOR << std::endl;
+	return (0);
+}
